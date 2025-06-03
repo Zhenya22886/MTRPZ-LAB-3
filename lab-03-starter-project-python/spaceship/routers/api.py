7be7841f493da7409bd624ca31d94e3ce776c214
Zhenya <sunak.zhenya@lll.kpi.ua>
2025-06-03 19:48:51 +0300

    created endpoint and rewrited dockerfile

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
@@ -6,3 +7,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/matrix-mult")
+async def matrix_mult():
+    a = numpy.random.rand(10, 10)
+    b = numpy.random.rand(10, 10)
+    product = a.dot(b)
+    return {
+        "matrix_a": a.tolist(),
+        "matrix_b": b.tolist(),
+        "product": product.tolist(),
+    }
